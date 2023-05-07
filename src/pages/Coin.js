5f04661adba5e41e6852b8269d9ea6888808822b
AliveOrdinary <19sama18@uohyd.ac.in>
2023-05-07 10:29:30 +0530

    chart implemented

--- src/pages/Coin.js
+++ src/pages/Coin.js
@@ -7,12 +7,15 @@ import List from "../components/Dashboard/List";
 import CoinInfo from "../components/Coin/CoinInfo";
 import { getCoinData } from "../functions/getCoinData";
 import { getCoinPrices } from "../functions/getCoinPrices";
+import LineChart from "../components/Coin/LineChart";
+import { convertDate } from "../functions/convertDate";
 
 const CoinPage = () => {
   const { id } = useParams();
   const [isLoading, setIsLoading] = useState(true);
   const [coinData, setCoinData] = useState();
   const [days, setDays] = useState(30);
+  const [chartData, setChartData] = useState({});
   useEffect(() => {
     if (id) {
       getData();
@@ -26,6 +29,20 @@ const CoinPage = () => {
       const prices = await getCoinPrices(id, days);
       if (prices.length > 0) {
         console.log("prices", prices);
+        setChartData({
+          labels: prices.map((price) => convertDate(price[0])),
+          datasets: [
+            {
+              data: prices.map((price) => price[1]),
+              borderColor: "#3a80e9",
+              borderWidth: 2,
+              fill: true,
+              tension: 0.15,
+              backgroundColor: "rgba(58, 128, 233, 0.1)",
+              pointRadius: 0,
+            },
+          ],
+        });
         setIsLoading(false);
       }
     }
@@ -39,6 +56,9 @@ const CoinPage = () => {
         <>
           <div className="grey-wrapper">
             <List coin={coinData} />
+          </div>
+          <div className="grey-wrapper">
+            <LineChart chartData={chartData} />
           </div>
           <CoinInfo heading={coinData.name} desc={coinData.desc} />
         </>
